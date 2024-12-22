c9a9d5bb2410da0183d32c5793066148183a1213
Distributed-Dave <dave@distributed.com>
2024-12-22 11:14:44 +0000

    fixing initial runtimes

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
@@ -14,7 +16,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/ara-kill-clock' : ''
+			base: dev ? '' : '/ara-kill-clock'
 		}
 	}
 };
